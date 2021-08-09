2836b0fb1492be2a6f22e89b225d2a3409b42469
Juan Pablo Castano <jpcastanom@gmail.com>
2021-08-09 12:59:35 -0500

    car crud in server

--- controllers/carController.js
+++ controllers/carController.js
@@ -8,3 +8,36 @@ exports.getCars = async (req, res) => {
     console.log("Error fetching all cars", error.message)
   }
 }
+
+exports.createCar = async (req, res) => {
+  const { car } = req.body
+  try {
+    const newCar = await Car.create(car)
+    res.json(newCar)
+  } catch (error) {
+    console.log("Error creating car in db: ", error.message)
+  }
+}
+
+exports.editCar = async (req, res) => {
+  const { carId, car } = req.body
+  try {
+    const editedCar = await Car.findByIdAndUpdate(carId, car, { new: true })
+    res.json(editedCar)
+  } catch (error) {
+    console.log("Error editing car in db: ", error.message)
+  }
+}
+
+exports.deleteCar = async (req, res) => {
+  const { carId } = req.body
+  try {
+    const deletedCar = await Car.findByIdAndDelete(carId)
+    res.json({
+      message: "Successfuly delete car",
+      deletedCar,
+    })
+  } catch (error) {
+    console.log("Error deleting car in db: ", error.message)
+  }
+}
--- routes/cars.js
+++ routes/cars.js
@@ -4,5 +4,8 @@ const router = express.Router()
 const carController = require("./../controllers/carController")
 
 router.get("/", carController.getCars)
+router.post("/create", carController.createCar)
+router.post("/edit", carController.editCar)
+router.post("/delete", carController.deleteCar)
 
 module.exports = router
